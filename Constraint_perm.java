c36e668ff7f764554ae314c5a7e1d9e3e62ff536
vinayparekar <vinayparekar24@gmail.com>
2015-03-10 21:30:44 -0500

    Create Constraint_perm.java
     CS 6301.{013,016}: Implementation of advanced DS&A
    Project 2 (Type 1: Enumeration of permutations and combinations)
    
    Ver 1.0: Thu, Sep 4
    
    Deadlines: 2 AM on Monday: Sep 15 (1st), Sep 22 (2nd), Sep 29 (3rd).
    
    Rules: Do not use code from others for this project.  You are free
    to use any source of information, printed or online, except for ones that
    have the code for solving these problems.  Any pseudocode or code given
    in class may be used as a starting point for your code, and it will not be
    considered as plagiarism.
    
    For each of the following problems, write a function "visit," with code
    similar to the following.  It visits a permutation/combination, and prints
    it, if the parameter "verbose" is nonzero.  Modify the code as needed.
    
    int visit(A, n, verbose, count)
    {
       if (verbose > 0) {
          for(int i = 0; i < n; i++) {
    	 if (A[i] > 0) System.out.print(A[i] + " ");
          }
          System.out.println();
       }
       return count+1;
    }
    
    Problems
    --------
    1. Write a program that visits all permutations/combinations of k objects
       from a set of n distinct objects, numbered 1..n.  The program takes
       input from the console/terminal/stdin and prints output to stdout.
    
       Input specification:
       Each line of input has 3 integers: n, k, and v.
       n is between 3 and 1000;  k is an integer between 0 and n; v is in {0,1,2,3}.
    
       Output specification:
       if v = 0, the program outputs one line with 2 integers: the number of
       permutations of k objects out of n distinct objects, and the time taken
       by the program in milliseconds (rounded to integer).  Even though the
       program is not printing the permutations, it should visit all permutations.
    
       if v = 1, the program again outputs one line with 2 integers, like the
       above case, except it outputs the number of combinations of k out of n.
    
       if v = 2 (verbose output version of v=0), the program should output all
       the actual permutations, one line at a time, and then the output of v=0.
    
       if v = 3 (verbose output of v=1), output all the actual combinations,
       and then the output of v=1.
    
       Sample input:
       4 2 2
    
       Sample output:
       1 2
       1 3
       1 4
       2 1
       2 3
       2 4
       3 1
       3 2
       3 4
       4 1
       4 2
       4 3
       12 0
       
    
    2. Write a program that visits all permutations of n (not necessarily
       distinct) objects.  The program reads from its standard input (stdin)
       and writes the output to stdout.
    
       Input specification:
       First line of input has 2 integers, n and v.
       The value of n is between 0 and 1000;  v is either 0 or 1.
       The next n integers in the input (separated by spaces and/or newlines)
       are the n elements.
    
       Output specification:
       If v > 0, then print each distinct permutation in one line of output.
       If v = 0, only one line of output is generated (below).  But your
       program should visit all the permutations, though it does not output them.
       The last line of output has the number of permutations visited and the
       time taken by the program in milliseconds (rounded to an integer).
    
       Sample input:
       4 1
       1 2 3 2
    
       Sample output:
       1 2 2 3
       1 2 3 2
       1 3 2 2
       2 1 2 3
       2 1 3 2
       2 2 1 3
       2 2 3 1
       2 3 1 2
       2 3 2 1
       3 1 2 2
       3 2 1 2
       3 2 2 1
       12 0

---
+++ Constraint_perm.java
@@ -0,0 +1,98 @@
+
+/* vinay parekar (vpp130130)
+ * 
+ * @author Vinay Parekar
+ * @email-id vpp130130@utdallas.edu
+ * @version 1.0
+ * 
+ * Code for permutations satisfying precedence constraints
+ *
+ */
+
+
+public class Constraint_perm {			// public class to compute permutation with constraints
+public static int count;				// To count no permutations with constraints
+public static int size,constr,verb;  	// Size of array, constr - no of constraint pair, verb - verbose option
+public static int[] constr_array; 		// constraint array for precedence of two values
+public static int[] A; 					// Global array to store array values
+
+public static void visit(int v){		// Visit function with verbose input to calculate permutation count and display permutations 
+	if(v==0){							// when verbose == o, calculate count alone. 
+		count++;						// count increment
+	}else if(v>0){						// when verbose > o, calculate count and different permutations.
+		for(int k=0; k<size ; k++){
+			if(A[k] != 0){
+				System.out.print(A[k]);
+			}
+		}
+		count++;						//count increment
+		System.out.println();
+	}	
+}
+
+public static void permute(int[] A,int i,int v){  // calculates different permutations  
+	if(i == 0){							// when A[] contains all values for a perticular permutation.
+		boolean flag = true;			// flag set to true
+		int target;						// to get precedence pair values
+		for(int k=0;k<constr*2;k=k+2){	// Loop to iterate through Constraint Array
+			target = constr_array[k];	// target set to first value of precedence 
+			if(flag == false){ 			// if flag is still set to false then did not follow the last constraint ,do not go visit 
+				flag=true;
+				return;}
+			for(int j= 0;j<size ;j++){	// loop to iterate through A[]
+				if(A[j] == target){		// target value found in A[]
+					target = constr_array[k+1];		// target value set to next value in same precedence pair
+					if(flag == true){				// if flag is true , set it to false
+						flag = false;
+					}else{							//if flag is false , set it to true
+						flag = true;
+					}
+				}
+				
+			}
+		}
+		if(flag == false){ 				// if flag is still set to false then did not follow the last constraint ,do not go visit
+			return;}					// return do not visit
+		visit(v);						// all constraint passed, can go visit	
+	}
+	else {
+	for(int k=0; k<size ; k++){		// code to generate permutations recursively
+		if(A[k] == 0){
+			A[k]=i;
+			permute(A, i-1,v);
+			A[k]=0;
+		}
+	}
+	}
+}
+
+
+public static void main(String[] args) {
+	 size = Integer.parseInt(args[0]);					// set Size 
+	 constr = Integer.parseInt(args[1]);				// set number of constraint value 
+	 verb = Integer.parseInt(args[2]);					// set value of verbose input
+	 
+	 if(verb < 0 ){										// check for invalid verbose input
+		 System.out.println("Invalid verbose input");
+		 System.exit(0);
+	 }
+	 
+	 constr_array = new int[constr*2];					// loop to set constraint precedence pair
+	 for(int i=0;i<constr*2;i++){
+		 constr_array[i] = Integer.parseInt(args[i+3]); 
+	 }
+	 
+	 
+	 A= new int[size];
+	 for(int i=0; i<size ; i++){						// initialize the A[]
+		 A[i] = 0;
+	 }
+	 
+	 long start = System.currentTimeMillis();
+	 permute(A,size,verb);								// call to permutation function
+	 long last = System.currentTimeMillis();
+	 System.out.println("Count:" + count);					// Display count
+	 System.out.println("Execution Time:" + (last-start));	// Execution time for the program
+	 
+}
+}
